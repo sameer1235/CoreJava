2de5aad9fe83b0251af8ee39f667d4310d0c1a47
Good Luck <sam123msr@gmail.com>
2022-02-02 21:57:03 +0530

    CoreJava first code commit.

---
+++ CoreJava/src/learning/Democode.java
@@ -0,0 +1,10 @@
+package learning;
+
+public class Democode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Firt code for CoreJava");
+	}
+
+}
---
+++ CoreJava/src/learning/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Good Luck
+ *
+ */
+package learning;
